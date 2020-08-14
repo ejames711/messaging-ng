bb79f4bd64a2642cd2d113d7144314f18cc0169a
Ethan Palas <ejames711@gmail.com>
2020-08-13 20:40:33 -0600

    first iteration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageDisplayComponent } from './message-display/message-display.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageDisplayComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
